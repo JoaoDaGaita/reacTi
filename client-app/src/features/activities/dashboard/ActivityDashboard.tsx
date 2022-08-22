d09ba49f092d66b1de806325c8cba98278f71334
JoaoDaGaita <joaovsilva70@outlook.com>
2022-08-22 13:47:58 -0300

    ENd of Section 9

--- client-app/src/features/activities/dashboard/ActivityDashboard.tsx
+++ client-app/src/features/activities/dashboard/ActivityDashboard.tsx
@@ -4,6 +4,7 @@ import { Grid } from 'semantic-ui-react';
 import { useStore } from '../../../app/stores/store';
 import { ActivityList } from './ActivityList';
 import { LoadingComponent } from '../../../app/layout/LoadingComponent';
+import { ActivityFilters } from './ActivityFilters';
 
 
 export const ActivityDashboard = observer (() => {
@@ -23,7 +24,7 @@ export const ActivityDashboard = observer (() => {
                 <ActivityList />
             </Grid.Column>
             <Grid.Column width='6'>
-                <h2>Activity filters</h2>
+                <ActivityFilters />
             </Grid.Column>
         </Grid>
     )
